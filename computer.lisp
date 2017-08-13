b5ae1b571e938ae4bf25217a3f3bf70aa9e0f647
CrazedProgrammer <crazedprogrammer@gmail.com>
2017-08-13 15:03:43 +0200

    Properly implement sandbox

--- computer.lisp
+++ computer.lisp
@@ -1,36 +1,56 @@
-(import lua/basic (_G load))
+(import lua/basic (_G load setmetatable))
 (import lua/coroutine coroutine)
-(import bindings/fs fs)
-(import util (deep-copy))
 (import base (set-idx!))
+(import bindings/fs fs)
+(import bindings/window window)
+(import bindings/term term)
+(import util (deep-copy))
 
-(defun create ()
+(defun create (boot-file)
   (let* [(cid 0)
          (label $"computer-${cid}")
-         (env (deep-copy _G))
-         (blacklist
-           '( ;"term" "gps" "parallel" "peripheral" "settings"
-              "colors" "colours" "disk" "help"
-              "io" "keys" "paintutils"
-              "rednet" "textutils" "vector" "window" ))
+         (env (create-env))
          (computer { :id cid
                      :label label
-                     :env env })]
-    (.<! env :getmetatable (lambda (a)
-                             (if (= (type a) "string")
-                               {}
-                               (.> env :getmetatable))))
-    (for-each entry blacklist
-      (set-idx! env entry nil))
+                     :env env
+                     :boot-file boot-file})]
     (.<! computer :coroutine
                   (coroutine/create (lambda () (run computer))))
     computer))
 
 (defun run (computer) :hidden
-  (let* [(handle (fs/open "bios.lua" "r"))
+  (let* [(handle (fs/open (.> computer :boot-file) "r"))
          (code ((.> handle :readAll)))]
     ((.> handle :close))
     ((load code "ccjam-bios.lua" "t" (.> computer :env)))))
 
 (defun next (computer args)
-  (coroutine/resume (.> computer :coroutine) (unpack args)))
+  (with (result (list (coroutine/resume (.> computer :coroutine) (unpack args))))
+    (if (= (car result) false)
+      (error! (.. "computer panicked! error: \n" (cadr result)))
+      (unpack (cddr result)))))
+
+
+(define api-blacklist :hidden
+ { ; "gps" "parallel" "peripheral" "settings"
+    :colors '() :colours '() :disk '() :help '()
+    :io '() :keys '() :paintutils '() :term '() :shell '()
+    :rednet '() :textutils '() :vector '() :window '()
+    :os '("shutdown") })
+
+(defun create-env () :hidden
+  (let* [(global (deep-copy _G))
+         (env (setmetatable { :_G global } { :__index global }))
+         (window (window/create (term/current) 1 1 (term/getSize)))]
+    (for-each api-name (keys api-blacklist)
+      (if (= (n (.> api-blacklist api-name)) 0)
+        (set-idx! global api-name nil)
+        (for-each api-item (.> api-blacklist api-name)
+          (set-idx! (.> global api-name) api-item nil))))
+    (.<! global :getmetatable
+             (lambda (a)
+               (if (= (type a) "string")
+                 {}
+                 (.> global :getmetatable))))
+    (.<! global :term { :native (lambda () window) })
+    env))
--- main.lisp
+++ main.lisp
@@ -1,6 +1,6 @@
 (import computer)
 (import bindings/os os)
 
-(with (comp (computer/create))
+(with (comp (computer/create "bios.lua"))
   (while true
     (computer/next comp (list (os/pullEvent)))))
