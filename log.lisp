c058e17b41450457a762ba5e96f0809e30a09625
CrazedProgrammer <crazedprogrammer@gmail.com>
2018-11-17 14:03:46 +0100

    Fix incorrect checking for logfile

--- log.lisp
+++ log.lisp
@@ -4,7 +4,7 @@
 (import config (args))
 
 (defun log! (message)
-  (when (/= (.> args :log-file) nil)
+  (when (.> args :log-file)
     (let* [(clock (os/clock))
            (time (.. (math/floor clock) "." (string/format "%02d" (* 100 (- clock (math/floor clock))))))]
       (append-all! (shell/resolve (.> args :log-file)) (format nil "[{#time}] {#message}\n")))))
