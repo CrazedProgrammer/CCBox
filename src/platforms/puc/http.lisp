617d3917a886990db8a19d5f1880882a29628b4d
CrazedProgrammer <crazedprogrammer@gmail.com>
2019-03-23 23:49:31 +0100

    Implement HTTP response header recapitilisation so pastebin.lua doesn't break

--- src/platforms/puc/http.lisp
+++ src/platforms/puc/http.lisp
@@ -1,6 +1,16 @@
 (import util (run-program! log!))
 (import computer/event event)
 
+(defun recapitalise-header-name (raw-name) :hidden
+  ;; TODO: implement all special cases that CC expects to be handled
+  ;; See https://github.com/netty/netty/blob/00afb19d7a37de21b35ce4f6cb3fa7f74809f2ab/codec-http/src/main/java/io/netty/handler/codec/http/HttpHeaders.java
+  (concat
+    (map (lambda (word)
+           (.. (string/upper (string/sub word 1 1))
+               (string/sub word 2 -1)))
+         (string/split raw-name "%-"))
+    "-"))
+
 (defun request (computer)
   (lambda (url post headers binary)
     (let* [(all-response (run-program! (.. "curl -sD - \"" url "\"")))
@@ -8,13 +18,18 @@
            (status-code (tonumber (cadr (string/split all-headers " "))))
            (headers
              (assoc->struct
-               (map (cut string/split <> "%: ")
+               (map (lambda (line)
+                      (let* [(parts (string/split line "%: "))
+                             (raw-name (car parts))
+                             (value (cadr parts))]
+                        (list (recapitalise-header-name raw-name) value)))
                     (cdr (string/split all-headers "\r\n")))))
            (response (string/sub all-response (+ (string/find all-response "\r\n\r\n") 4) -1))
            (handle
              { :readAll (const response)
                :getResponseCode (const status-code)
-               :getResponseHeaders (const headers) })]
+               :getResponseHeaders (const headers)
+               :close (const nil)})]
       ; TODO: implement other HTTP methods and request headers
       (event/queue! computer (list "http_success" url handle))
       true)))
