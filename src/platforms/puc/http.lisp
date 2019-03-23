b4f95dc15b0d5881987e9948ef9b775974e0847c
CrazedProgrammer <crazedprogrammer@gmail.com>
2019-03-23 23:14:23 +0100

    Add getResponseCode and getResponseHeaders to PUC HTTP implementation

--- src/platforms/puc/http.lisp
+++ src/platforms/puc/http.lisp
@@ -3,7 +3,18 @@
 
 (defun request (computer)
   (lambda (url post headers binary)
-    ; TODO: implement all other features
-    (event/queue! computer (list "http_success" url { :readAll (lambda ()
-                 (run-program! (.. "curl -s \"" url "\""))) }))
-    true))
+    (let* [(all-response (run-program! (.. "curl -sD - \"" url "\"")))
+           (all-headers (string/sub all-response 1 (- (string/find all-response "\r\n\r\n") 1)))
+           (status-code (tonumber (cadr (string/split all-headers " "))))
+           (headers
+             (assoc->struct
+               (map (cut string/split <> "%: ")
+                    (cdr (string/split all-headers "\r\n")))))
+           (response (string/sub all-response (+ (string/find all-response "\r\n\r\n") 4) -1))
+           (handle
+             { :readAll (const response)
+               :getResponseCode (const status-code)
+               :getResponseHeaders (const headers) })]
+      ; TODO: implement other HTTP methods and request headers
+      (event/queue! computer (list "http_success" url handle))
+      true)))
