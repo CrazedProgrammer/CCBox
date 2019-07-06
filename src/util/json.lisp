5e3971e8cf954c372654b34cd1fe0d9f7c8fdd13
CrazedProgrammer <crazedprogrammer@gmail.com>
2019-07-06 16:30:46 +0200

    Implement json encode

--- src/util/json.lisp
+++ src/util/json.lisp
@@ -1,6 +1,6 @@
 (import cli (cli-args))
 (import util/embed (embedded-json))
-(import lua/basic (load))
+(import lua/basic (load type#))
 (import util/io (resolve-path))
 (import io (read-all!))
 
@@ -12,8 +12,38 @@
     "json.lua"
     "t")))
 
-(defun encode (data)
-  ((.> json :encode) data))
-
 (defun decode (str)
   ((.> json :decode) str))
+
+; TODO: Make a lookup table for this.
+(defun char->quoted (code)
+  (cond [(= code #x22) "\\\""]
+        [(= code #x5C) "\\\\"]
+        [(= code #x0A) "\\n"]
+        [(= code #x0D) "\\r"]
+        [(< code #x20) (string/format "\\u%04x" code)]
+        [else (string/char code)]))
+
+(defun quoted (str)
+  (.. "\""
+      (string/concat
+        (map (lambda (c)
+               (char->quoted (string/byte c)))
+             (string/split str "")))
+      "\""))
+
+(defun assoc->str (assoc)
+  (.. (quoted (car assoc))
+      ":"
+      (encode (cadr assoc))))
+
+; TODO: Optimise.
+(defun encode (data)
+  (if (= (type# data) "string")
+    (quoted data)
+    (.. "{"
+        (string/concat
+          (map assoc->str
+               (struct->assoc data))
+          ",")
+        "}")))
